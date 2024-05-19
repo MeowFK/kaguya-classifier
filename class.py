0ff790b9f710de87fef67dbb67c78d01d5df6330
MeowFK <albertyzhang429@gmail.com>
2024-05-19 16:04:03 -0400

    env test
    gonna reopen to see if the env variable finally set

--- class.py
+++ class.py
@@ -8,4 +8,12 @@ if torch.backends.mps.is_available():
 else:
     device = torch.device('cpu')
 
-# use 
+# use Azure search key
+azure_search_key = os.getenv('AZURE_IMAGE_SEARCH_KEY')
+
+if not azure_search_key:
+    raise Exception("Set azure key in environment")
+print(azure_search_key)
+key = os.environ.get('AZURE_SEARCH_KEY', azure_search_key)
+
+
