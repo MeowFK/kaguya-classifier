c51a455b7c23b8055c9d5bc2eef93a478b2c27da
MeowFK <albertyzhang429@gmail.com>
2024-05-19 22:48:04 -0400

    images images images
    got a lot of images from bing :)

--- class.py
+++ class.py
@@ -1,7 +1,9 @@
 # if there's problems here, update fastai and torch to latest versions
 # pip install 'name' == 'version'
+# fastbook needed for bing_image_search
 import dotenv
 from fastai.vision.all import *
+from fastbook import *
 
 # remember to set dls.device = device and learn.model.to(device)
 if torch.backends.mps.is_available():
@@ -13,11 +15,48 @@ else:
 dotenv.load_dotenv()
 
 # use Azure search key
-azure_search_key = os.getenv('AZURE_IMAGE_SEARCH_KEY')
+image_key = os.getenv('AZURE_IMAGE_SEARCH_KEY')
 
-if not azure_search_key:
+if not image_key:
     raise Exception("Set azure key in environment")
-print(azure_search_key)
-key = os.environ.get('AZURE_SEARCH_KEY', azure_search_key)
 
+# search up images, probably needs a more specific search...
+# kaguya_images = search_images_bing(image_key, 'Shinomiya Kaguya')
+# ims = kaguya_images.attrgot('contentUrl')
 
+# looking at an image
+# dest = 'images/kaguya.jpg'
+# download_url(ims[0], dest)
+# im = Image.open(dest)
+# im.to_thumb(128,128)
+
+characters = ['shinomiya kaguya', 
+              'miyuki shirogane', 
+              'fujiwara chika', 
+              'yu ishigami', 
+              'miko iino', 
+              'ai hayasaka', 
+              'shirogane kei']
+path = Path('kaguyasama images')
+
+# make folders for characters
+if not path.exists():
+    path.mkdir()
+    for o in characters:
+        dest = path/o 
+        dest.mkdir(exist_ok = True) # ok if exists, leave unaltered
+        results = search_images_bing(image_key, f"{o}") # search images
+        urls = results.attrgot('contentUrl') # get list of urls for images
+        download_images(dest, urls=urls) # download images into characters folder
+
+# get image files and unlink failed ones
+images = get_image_files(path)
+failed = verify_images(fns = images)
+failed.map(Path.unlink)
+
+# just checking the number of images which remain after the carnage
+# for o in characters:
+#     img_list = os.listdir(path/o)
+#     print(f'{o}: {len(img_list)}')
+
+# make DataBlock
